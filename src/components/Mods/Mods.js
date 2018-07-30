f05ea0b5a44eecf849de3f0ae7b8f937598b24d4
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-07-30 16:37:44 +0300

    fix

--- src/components/Mods/Mods.js
+++ src/components/Mods/Mods.js
@@ -2,28 +2,31 @@ import React from 'react';
 
 const Mods = (props) => {
     return (
-            <table className="table table-bordered">
-                <thead>
-                <tr>
-                    <th>Двигатель</th>
-                    <th>КПП</th>
-                    <th>Привод</th>
-                    <th className="d-none d-sm-block">Разгон до 100</th>
-                    <th className="d-none d-sm-block">Расход</th>
+        <table className="table table-bordered">
+            <thead>
+            <tr>
+                <th>Двигатель</th>
+                <th>КПП</th>
+                <th>Привод</th>
+                <th className="d-none d-sm-table-cell">Разгон до 100</th>
+                <th className="d-none d-sm-table-cell">Расход</th>
+            </tr>
+            </thead>
+            <tbody>
+            {props.mods.reduce((prev, next) => {
+                if (prev.findIndex((el) => el.engine + el.gearbox === next.engine + next.gearbox) === -1) return [...prev, next];
+                else return prev;
+            }, []).sort((a, b) => a.engine < b.engine ? -1 : 1).map(mod =>
+                <tr key={mod.id}>
+                    <td>{mod.engine}</td>
+                    <td>{mod.gearbox}</td>
+                    <td>{mod.drive}</td>
+                    <td className="d-none d-sm-table-cell">{mod.accel}</td>
+                    <td className="d-none d-sm-table-cell">{mod.consum}</td>
                 </tr>
-                </thead>
-                <tbody>
-                {props.mods.sort((a, b) => a.engine < b.engine ? -1 : 1).map(mod =>
-                    <tr key={mod.id}>
-                        <td>{mod.engine}</td>
-                        <td>{mod.gearbox}</td>
-                        <td>{mod.drive}</td>
-                        <td className="d-none d-sm-block">{mod.accel}</td>
-                        <td className="d-none d-sm-block">{mod.consum}</td>
-                    </tr>
-                )}
-                </tbody>
-            </table>
+            )}
+            </tbody>
+        </table>
     )
 };
 
