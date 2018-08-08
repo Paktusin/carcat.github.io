8643f53ea646b1a23a20ebadcbe2471db568ce23
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2018-08-08 09:22:16 +0300

    mb

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import './css/audi.scss';
 import './css/bmw.scss';
+import './css/mercedes-benz.scss';
 import App from './components/App/App';
 
 ReactDOM.render(<App/>, document.getElementById('root'));
