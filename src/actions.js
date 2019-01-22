7a31ad466b31259a3d51fffcd45cb89e50e22ec1
davlichin.ma <davlichin.ma@talantiuspeh.ru>
2019-01-22 12:20:13 +0300

    fix api

--- src/actions.js
+++ src/actions.js
@@ -7,7 +7,7 @@ export const actions = {
     SET_BRAND: 'SET_BRAND',
     SET_MODELS: 'SET_MODELS',
     SET_MODEL: 'SET_MODEL',
-    API_URL: 'https://carcat.paktusin.beget.tech:8081/'
+    API_URL: 'https://paktusin.ddns.net:8081/'
 };
 
 const setBodyClass = (name) => {
