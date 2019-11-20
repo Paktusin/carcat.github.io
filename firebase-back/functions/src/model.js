1d2c8be2c3b41a01a86cdddd7b729d593243d327
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-11-20 17:51:20 +0300

    add firebase back

--- firebase-back/functions/src/model.js
+++ firebase-back/functions/src/model.js
@@ -2,16 +2,16 @@ const router = require('express').Router();
 const database = require('./database');
 
 router.get('/', (req, res, next) => {
-    const brand_id = parseInt(req.query.brand_id)
+    const brand_id = parseInt(req.query.brand_id);
     if (!brand_id) return next();
-    database.where('model', 'brand_id', brand_id).then(snapshot => {
-        return res.json(Object.values(snapshot.val()))
+    database.where('models', 'brand_id', brand_id).then(snapshot => {
+        return res.json(snapshot.val().values())
     })
 });
 
 router.get('/:id', (req, res) => {
-    database.find('model', req.params.id).then(snapshot => {
-        return res.json(snapshot.val().values())
+    database.find('models', req.params.id).then(snapshot => {
+        return res.json(snapshot.val())
     })
 });
 
