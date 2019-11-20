0b465e1d19b8cc0ad0d347fdad212c020c389d6a
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-11-20 17:28:26 +0300

    add firebase back

--- back/mysqlToFirebase.js
+++ back/mysqlToFirebase.js
@@ -9,7 +9,7 @@ var con = mysql.createConnection({
 });
 
 const folder = 'migrates';
-const tables = ['body', 'brand', 'gen', 'model', 'modif'];
+const tables = ['brand', 'modif'];
 if (!fs.existsSync(folder)) fs.mkdirSync(folder);
 dump();
 
@@ -18,13 +18,38 @@ function dump() {
         const dump = {};
         if (err) throw err;
         console.log("Connected!");
-
         for (let table of tables) {
-            const values = await new Promise(resolve => con.query(`select * from ${table}`, function (err, result) {
+            dump[table] = await new Promise(resolve => con.query(`select * from ${table}`, function (err, result) {
                 if (err) throw err;
-                resolve(result.reduce((prev, obj) => ({...prev, [obj.id]: obj}), {}));
+                if (table === 'brand')
+                    resolve(result.reduce((prev, obj) => ({...prev, [obj.id]: obj}), {}));
+                else
+                    resolve(result)
             }));
+            console.log(table);
         }
+
+        const models = await new Promise(resolve => con.query(`select * from model`, function (err, result) {
+            resolve(result.reduce((prev, obj) => ({...prev, [obj.id]: obj}), {}));
+        }));
+        const gens = await new Promise(resolve => con.query(`select * from gen`, function (err, result) {
+            resolve(result);
+        }));
+        const bodies = await new Promise(resolve => con.query(`select * from body`, function (err, result) {
+            resolve(result);
+        }));
+        Object.keys(models).forEach(key => {
+            const model_id = parseInt(key);
+            models[key].gens = [];
+            for (let gen of gens.filter(gen => gen.model_id === model_id)) {
+                gen.bodies = [];
+                for (let body of bodies.filter(body => body.gen_id === gen.id)) {
+                    gen.bodies.push(body);
+                }
+                models[key].gens.push(gen);
+            }
+        });
+        dump.models = models;
 
         const path = `${folder}/dump.json`;
         fs.writeFile(path, JSON.stringify(dump), (err) => {
