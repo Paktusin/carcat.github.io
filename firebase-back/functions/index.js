40a968da51885396635984e6589cb657c2920d57
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-11-20 14:35:13 +0300

    add firebase back

--- firebase-back/functions/index.js
+++ firebase-back/functions/index.js
@@ -2,19 +2,27 @@ const functions = require('firebase-functions');
 const express = require('express');
 const admin = require('firebase-admin');
 
-let serviceAccount = require("../key");
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.applicationDefault(),
     databaseURL: "https://carcat-3a3fb.firebaseio.com"
 });
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.set('Cache-Control', 'public, max-age=300');
+    return next();
+});
+
 app.get('/brand', (req, res) => {
     admin.database().ref('brand/').once('value').then(snapshot => {
-        console.log(snapshot.val());
         return res.json(snapshot.val())
     })
+});
+
+app.use((req, res, next) => {
+    res.statusCode = 404;
+    return res.send();
 });
 
 exports.carcat = functions.https.onRequest(app);
