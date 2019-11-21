1ba3cc35601630a9a37837ccd26f9d13c0ce36bc
mikhail.davlichin <mikhail.davlichin@kaspersky.com>
2019-11-21 13:52:10 +0300

    Updates

---
+++ precache-manifest.6fae1cb89bdca4c718089e9113c76a42.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14e91800734f5025428f43792b266be6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2c774501d107ade16472",
+    "url": "/static/css/main.72865900.chunk.css"
+  },
+  {
+    "revision": "55d50a907ec348bf3e2b",
+    "url": "/static/js/2.9e4e2414.chunk.js"
+  },
+  {
+    "revision": "2c774501d107ade16472",
+    "url": "/static/js/main.49704792.chunk.js"
+  },
+  {
+    "revision": "c636e4013e074309cb78",
+    "url": "/static/js/runtime-main.6f415da0.js"
+  },
+  {
+    "revision": "ca2613713ba8f8600c479dea000c58f5",
+    "url": "/static/media/AudiType-ExtendedBold.ca261371.woff"
+  },
+  {
+    "revision": "51107c3742b23404a83cfa4c863abe8d",
+    "url": "/static/media/AudiType-ExtendedNormal.51107c37.woff"
+  },
+  {
+    "revision": "7f4a3c8d97a37cbe23da1346977a39df",
+    "url": "/static/media/BMWTypeWebBold.7f4a3c8d.woff2"
+  },
+  {
+    "revision": "4a40a6a5eca47bf775f3aafaae781e69",
+    "url": "/static/media/BMWTypeWebLight.4a40a6a5.woff2"
+  },
+  {
+    "revision": "b5b750900832b055cb8fe554b0d5d7ed",
+    "url": "/static/media/Daimler.b5b75090.woff2"
+  },
+  {
+    "revision": "f633967b49fdb1e5d8117f38b2da333f",
+    "url": "/static/media/DaimlerCS-Regular.f633967b.woff2"
+  }
+]);
